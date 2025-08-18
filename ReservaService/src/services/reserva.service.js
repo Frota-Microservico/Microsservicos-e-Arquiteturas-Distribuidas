f5464f5c4f746124ed49af71c89739232f52b961
Luis-gui-trentino <trentinopereira1@gmail.com>
2025-08-18 00:48:47 -0300

    Finalizando as rotas do Reserva

--- ReservaService/src/services/reserva.service.js
+++ ReservaService/src/services/reserva.service.js
@@ -1,3 +1,4 @@
+import { where } from "sequelize";
 import { ReservaModel } from "../../models/reserva.model.js";
 
 export class ReservaService {
@@ -22,4 +23,54 @@ export class ReservaService {
             data: reserva
         });
     }
+
+    static async getListarReservas(req, res) {
+
+        const reservasAtivas = await ReservaModel.findAll({});
+        return reservasAtivas;
+    }
+
+    static async getProcuraReservas(req, res) {
+        const id = parseInt(req.params.id, 10);
+
+        if (isNaN(id)) {
+            return res.status(400).json({ status: 400, detail: "ID inválido" });
+        }
+
+        try {
+            const reservaPorId = await ReservaModel.findByPk(id);
+            return reservaPorId;
+        } catch (error) {
+            console.log("Erro ao encontrar a reserva");
+            return null;
+        }
+    }
+
+    static async deleteReserva(id) {
+        const verificaReserva = await ReservaModel.findByPk(id);
+
+        if (!verificaReserva) {
+            return res.status(400).json({ status: 400, detail: "Não foi encontrado a reserva" });
+        }
+
+        await ReservaModel.destroy({
+            where: { id: id }
+        });
+
+        return true;
+    }
+
+    static async putReserva(id, dt_reserva, dt_devolucao) {
+
+        await ReservaModel.update(
+            {
+                dt_reserva: dt_reserva,
+                dt_devolucao: dt_devolucao 
+            },
+            { where: { id: id } }
+        );
+
+        return await ReservaModel.findByPk(id);
+    }
+
 }
